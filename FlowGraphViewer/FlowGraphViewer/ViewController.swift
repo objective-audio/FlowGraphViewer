2f6b0b2acde1b0c91c09d43850b30c2cf505bbc2
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-09 18:29:07 +0900

    no message

--- FlowGraphViewer/FlowGraphViewer/ViewController.swift
+++ FlowGraphViewer/FlowGraphViewer/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: NSViewController {
         self.pool += AppController.shared.url.chain().do { [weak self] url in self?.updateImage(with: url) }.sync()
     }
     
-    func updateImage(with url: URL?) {
+    private func updateImage(with url: URL?) {
         if let url = url, let image = NSImage(contentsOf: url) {
             self.imageView.frame = NSMakeRect(0, 0, image.size.width, image.size.height)
         } else {
