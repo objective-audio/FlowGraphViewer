59b1d2c904d08d5d9a29d132a5d65fc932954823
Yuki Yasoshima <yukiyasos@gmail.com>
2018-11-09 14:20:56 +0900

    no message

--- FlowGraphViewer/FlowGraphViewer/AppController.swift
+++ FlowGraphViewer/FlowGraphViewer/AppController.swift
@@ -2,12 +2,30 @@
 //  AppController.swift
 //
 
-import Foundation
+import Cocoa
+import Chaining
 
 class AppController {
     static let shared = AppController()
     
+    #warning("とりあえず画像のURLを持ってみる")
+    
     func open() {
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseDirectories = false
+        openPanel.canChooseFiles = true
+        openPanel.allowsMultipleSelection = false
+//        openPanel.allowedFileTypes = ["swift"]
         
+        openPanel.begin { response in
+            switch response {
+            case NSApplication.ModalResponse.OK:
+                print("ok : \(response)")
+            case NSApplication.ModalResponse.cancel:
+                print("cancel : \(response)")
+            default:
+                break
+            }
+        }
     }
 }
